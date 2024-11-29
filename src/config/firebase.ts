9a246a6d1ca82b4074b2359c967ba18f1aca7f31
chologoriops <chologoriops@gmail.com>
2024-11-29 12:38:44 +0600

    added the firebase api

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBup8jiT7xD0fmQbfwIAbNzlI32TpOBccg",
-  authDomain: "bazar-dor-cfcb2.firebaseapp.com",
-  projectId: "bazar-dor-cfcb2",
-  storageBucket: "bazar-dor-cfcb2.appspot.com",
-  messagingSenderId: "448061198099",
-  appId: "1:448061198099:web:de55e75a2943326cd214e6",
-  measurementId: "G-8S9FN3R4V7"
+  apiKey: "AIzaSyBYhYLMjzs8N_7BVnVV2y0R3wNTSU1EzFM",
+  authDomain: "bazar-dor-3.firebaseapp.com",
+  projectId: "bazar-dor-3",
+  storageBucket: "bazar-dor-3.firebasestorage.app",
+  messagingSenderId: "875906070898",
+  appId: "1:875906070898:web:d366ff34934a2c4a3a05eb"
 };
 
 // Initialize Firebase
